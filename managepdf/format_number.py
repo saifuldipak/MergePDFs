1aaa7e9078ae7c6eb5cd99daf78529ce9b603d23
Saiful Islam <saiful.kbtel@gmail.com>
2023-11-01 14:36:59 +0600

    Feat: add 'format_number.py' script

---
+++ managepdf/format_number.py
@@ -0,0 +1,24 @@
+# Define the input and output file paths
+input_file = "input.txt"
+output_file = "output.txt"
+
+# Open the input file for reading and the output file for writing
+with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+    for line in infile:
+        cleaned_line = ''
+        for char in line:
+            if char.isalpha():
+                continue
+            if char in (' ', '.', '-', ','):
+                char = ''
+            elif char in ('/'):
+                char = '1'
+            cleaned_line += char
+            
+           # print(cleaned_line, len(cleaned_line))
+
+        if len(cleaned_line.strip()) == 11:
+            output_line = f'88{cleaned_line}'
+            outfile.write(output_line)
+
+
