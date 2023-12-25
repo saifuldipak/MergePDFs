556cdb1b35842d5ae28efeb4e5cb132433b50f4d
Saiful Islam <saiful.kbtel@gmail.com>
2023-12-25 22:03:40 +0600

    Fix: check pattern when filtering

--- tools/format_number.py
+++ tools/format_number.py
@@ -1,22 +1,28 @@
+import re
+
 # Define the input and output file paths
-input_file = "input.txt"
-output_file = "output.txt"
+pattern = re.compile('^01')
+INPUT = "input.txt"
+OUTPUT = "output.txt"
 
 # Open the input file for reading and the output file for writing
-with open(input_file, "r") as infile, open(output_file, "w") as outfile:
+with open(INPUT, "r", encoding='utf-8') as infile, open(OUTPUT, "w", encoding='utf-8') as outfile:
     for line in infile:
         cleaned_line = ''
         for char in line:
             if char.isalpha():
-                continue
+                continue 
             if char in (' ', '.', '-', ','):
                 char = ''
             elif char in ('/'):
                 char = '1'
             cleaned_line += char
             
-        if len(cleaned_line.strip()) == 11:
-            output_line = f'88{cleaned_line}'
-            outfile.write(output_line)
-
-
+        if len(cleaned_line.strip()) != 11:
+            continue
+        
+        if not pattern.match(cleaned_line):
+            continue
+        
+        output_line = f'88{cleaned_line}'
+        outfile.write(output_line)
